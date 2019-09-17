b102cee6d3e0c30f58f133dd2a3b4ae62f4da3bd
lipengyue <pengyue.li@yi23.net>
2019-09-17 13:47:59 +0800

    【微博 接口】

---
+++ Example/PYJsonViewManager/Demo/JsonHandlerDemo/PYJsonString.h
@@ -0,0 +1,17 @@
+//
+//  PYJsonString.h
+//  PYJsonViewManager_Example
+//
+//  Created by 衣二三 on 2019/9/17.
+//  Copyright © 2019 LiPengYue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PYJsonString : NSObject
+@property (nonatomic,strong,class) NSString *sinaJsonStr;
+@end
+
+NS_ASSUME_NONNULL_END
